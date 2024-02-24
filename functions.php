1d05fe13bf80dc30d717356c3ee784204bc72a24
AmirHesam Piri <amirhesum@gmail.com>
2024-02-24 06:01:31 +0330

    Add Force SSL/HTTPS

--- functions.php
+++ functions.php
@@ -34,3 +34,9 @@ add_action('init', 'limit_heartbeat', 1);
         wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/custom.min.js', array('jquery'), null, true);
     }
     add_action('wp_enqueue_scripts', 'combine_and_minify_assets');
+    // Force SSL/HTTPS
+    if ($_SERVER['HTTPS'] !== 'on') {
+        wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
+        exit();
+    }
+    
